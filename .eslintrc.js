eba583e61f0c8b41899c40186ccccf4769f3a594
Mahmoud Mansy <115116883+MMansy19@users.noreply.github.com>
2025-06-02 06:05:35 +0000

    refactor: update ESLint configuration and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,34 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  plugins: ['react', 'react-hooks', 'tailwindcss', '@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
-    'plugin:tailwindcss/recommended',
   ],
-  overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'tailwindcss/classnames-order': 'off',
-    'tailwindcss/no-custom-classname': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/strict-boolean-expressions': [
-      'warn',
-      {
-        allowNullableObject: true,
-      },
-    ],
-    '@typescript-eslint/no-misused-promises': [
-      2,
-      {
-        checksVoidReturn: {
-          attributes: false,
-        },
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': 'off',
-    'no-callback-literal': 'off',
-    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'react/prop-types': 'off',
-    '@typescript-eslint/consistent-indexed-object-style': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
